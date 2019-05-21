b0089f5a1713efc89a736aacba84b29e4b04ed73
bishwa <karkeebishwa1@gmail.com>
2019-05-21 13:27:24 +0545

    image caption loader

--- glove_loader.py
+++ glove_loader.py
@@ -37,3 +37,56 @@ def load_glove(data_dir_path=None, embedding_dim=None):
         _word2em[word] = embeds
     file.close()
     return _word2em
+
+
+class GloveModel(object):
+    model_name = 'glove-model'
+
+    def __init__(self):
+        self.word2em = None
+        self.embedding_dim = None
+
+    def load(self, data_dir_path, embedding_dim=None):
+        if embedding_dim is None:
+            embedding_dim = 100
+        self.embedding_dim = embedding_dim
+        self.word2em = load_glove(data_dir_path, embedding_dim)
+
+    def encode_docs(self, docs, max_allowed_doc_lenght=None):
+        doc_count = len(docs)
+        X = np.zeros(shape=(doc_count, self.embedding_dim))
+        max_len = 0
+        for doc in docs:
+            max_len = max(max_len, len(doc.split(' ')))
+        if max_allowed_doc_lenght is not None:
+            max_len = min(max_len, max_allowed_doc_lenght)
+        for i in range(0, doc_count):
+            doc = docs[i]
+            words = [w.lower() for w in doc.split(' ')]
+            length = min(max_len, len(words))
+            E = np.zeros(shape=(self.embedding_dim, max_len))
+            for j in range(length):
+                word = words[j]
+                try:
+                    E[:, j] = self.word2em[word]
+                except KeyError:
+                    pass
+            X[i, :] = np.sum(E, axis=1)
+
+        return X
+
+    def encode_doc(self, doc, max_allowed_doc_length=None):
+        words = [w.lower() for w in doc.split(' ')]
+        max_len = len(words)
+        if max_allowed_doc_length is not None:
+            max_len = min(len(words), max_allowed_doc_length)
+        E = np.zeros(shape=(self.embedding_dim, max_len))
+        X = np.zeros(shape=(self.embedding_dim,))
+        for j in range(max_len):
+            word = words[j]
+            try:
+                E[:, j] = self.word2em[word]
+            except KeyError:
+                pass
+        X[:] = np.sum(E, axis=1)
+        return X
