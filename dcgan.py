0399cda3a5df694dc3f968286f551b328d277e87
bishwa <karkeebishwa1@gmail.com>
2019-05-22 13:01:04 +0545

    image utils and training process

--- dcgan.py
+++ dcgan.py
@@ -3,9 +3,12 @@ from keras.layers import Input, Dense, Reshape, concatenate
 from keras.layers.core import Activation, Flatten
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import UpSampling2D, Conv2D, MaxPooling2D
+from .image_utils import combine_normalized_images, img_from_normalized_img
 from keras import backend as K
 from keras.optimizers import SGD
 import os
+import numpy as np
+from PIL import Image
 
 from .glove_loader import GloveModel
 
@@ -101,3 +104,107 @@ class DCGan(object):
         self.model.compile(loss='binary_crossentropy', optimizer=g_optim)
 
         print('generator-discriminator: ', self.model.summary())
+
+    def load_model(self, model_dir_path):
+        config_file_path = DCGan.get_config_file_path(model_dir_path)
+        self.config = np.load(config_file_path).item()
+        self.img_width = self.config['img_width']
+        self.img_height = self.config['img_height']
+        self.img_channels = self.config['img_channels']
+        self.random_input_dim = self.config['random_input_dim']
+        self.text_input_dim = self.config['text_input_dim']
+        self.glove_source_dir_path = self.config['glove_source_dir_path']
+        self.create_model()
+        self.glove_model.load(self.glove_source_dir_path, embedding_dim=self.text_input_dim)
+        self.generator.load_weights(DCGan.get_weight_file_path(model_dir_path, 'generator'))
+        self.discriminator.load_weights(DCGan.get_weight_file_path(model_dir_path, 'discriminator'))
+
+    def fit(self, model_dir_path, image_label_pairs, epochs=None, batch_size=None, snapshot_dir_path=None,
+            snapshot_interval=None):
+        if epochs is None:
+            epochs = 100
+
+        if batch_size is None:
+            batch_size = 128
+
+        if snapshot_interval is None:
+            snapshot_interval = 20
+
+        self.config = dict()
+        self.config['img_width'] = self.img_width
+        self.config['img_height'] = self.img_height
+        self.config['random_input_dim'] = self.random_input_dim
+        self.config['text_input_dim'] = self.text_input_dim
+        self.config['img_channels'] = self.img_channels
+        self.config['glove_source_dir_path'] = self.glove_source_dir_path
+
+        self.glove_model.load(data_dir_path=self.glove_source_dir_path, embedding_dim=self.text_input_dim)
+
+        config_file_path = DCGan.get_config_file_path(model_dir_path)
+
+        np.save(config_file_path, self.config)
+        noise = np.zeros((batch_size, self.text_input_dim))
+        text_batch = np.zeros((batch_size, self.text_input_dim))
+
+        self.create_model()
+
+        for epoch in range(epochs):
+            print('Epoch is', epoch)
+            batch_count = int(image_label_pairs.shape[0] / batch_size)
+            print("Number of batches", batch_count)
+            for batch_index in range(batch_count):
+                image_label_pairs_batch = image_label_pairs[batch_index * batch_size:(batch_index + 1) * batch_size]
+
+                image_batch = []
+                for index in range(batch_size):
+                    image_label_pair = image_label_pairs_batch[index]
+                    normalized_img = image_label_pair[0]
+                    text = image_label_pair[1]
+                    image_batch.append(normalized_img)
+                    text_batch[index, :] = self.glove_model.encode_doc(text, self.text_input_dim)
+                    noise[index, :] = np.random.uniform(-1, 1, self.random_input_dim)
+
+                image_batch = np.array(image_batch)
+
+                generated_images = self.generator.predict([noise, text_batch], verbose=0)
+
+                if(epoch * batch_size + batch_index) % snapshot_interval == 0 and snapshot_dir_path is not None:
+                    self.save_snapshots(generated_images, snapshot_dir_path=snapshot_dir_path,
+                                        epoch = epoch, batch_index=batch_index)
+                self.discriminator.trainable = True
+                d_loss = self.discriminator.train_on_batch([np.concatenate((image_batch, generated_images)),
+                                                            np.concatenate((text_batch, text_batch))],
+                                                           np.array([1] * batch_size + [0] * batch_size))
+                print("Epoch %d batch %d d_loss : %f" % (epoch, batch_index, d_loss))
+
+                # Step 2: train the generator
+                for index in range(batch_size):
+                    noise[index, :] = np.random.uniform(-1, 1, self.random_input_dim)
+                self.discriminator.trainable = False
+                g_loss = self.model.train_on_batch([noise, text_batch], np.array([1] * batch_size))
+
+                print("Epoch %d batch %d g_loss : %f" % (epoch, batch_index, g_loss))
+                if (epoch * batch_size + batch_index) % 10 == 9:
+                    self.generator.save_weights(DCGan.get_weight_file_path(model_dir_path, 'generator'), True)
+                    self.discriminator.save_weights(DCGan.get_weight_file_path(model_dir_path, 'discriminator'), True)
+
+        self.generator.save_weights(DCGan.get_weight_file_path(model_dir_path, 'generator'), True)
+        self.discriminator.save_weights(DCGan.get_weight_file_path(model_dir_path, 'discriminator'), True)
+
+
+    def generate_image_from_text(self, text):
+        noise = np.zeros(shape=(1, self.random_input_dim))
+        encoded_text = np.zeros(shape=(1, self.text_input_dim))
+        encoded_text[0, :] = self.glove_model.encode_doc(text)
+        noise[0, :] = np.random.uniform(-1, 1, self.random_input_dim)
+        generated_images = self.generator.predict([noise, encoded_text], verbose=0)
+        generated_image = generated_images[0]
+        generated_image = generated_image * 127.5 + 127.5
+        return Image.fromarray(generated_image.astype(np.uint8))
+
+    def save_snapshots(self, generated_images, snapshot_dir_path, epoch, batch_index):
+        image = combine_normalized_images(generated_images)
+        img_from_normalized_img(image).save(
+            os.path.join(snapshot_dir_path, DCGan.model_name + '-' + str(epoch) + "-" + str(batch_index) + ".png"))
+
+
